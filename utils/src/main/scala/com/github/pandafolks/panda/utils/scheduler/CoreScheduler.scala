b832e34b0bdc2815fecff1796b0a2b0682d0e9b6
MattSzm <mateuszszmal@gmail.com>
2022-09-25 00:57:22 +0200

    #59 logging info about scheduler

--- utils/src/main/scala/com/github/pandafolks/panda/utils/scheduler/CoreScheduler.scala
+++ utils/src/main/scala/com/github/pandafolks/panda/utils/scheduler/CoreScheduler.scala
@@ -2,7 +2,22 @@ package com.github.pandafolks.panda.utils.scheduler
 
 import monix.execution.Scheduler
 import monix.execution.Scheduler.Implicits.global
+import org.slf4j.LoggerFactory
 
 object CoreScheduler {
   implicit final val scheduler: Scheduler = global
+
+  locally {
+    LoggerFactory.getLogger(getClass.getName).info(s"Running Panda on the ${getClass.getName} scheduler:" +
+      "\n[" +
+      s"\n\tavailableProcessors: ${Runtime.getRuntime.availableProcessors()}" +
+      s"\n\tSystem properties overrides:" +
+      s"\n\t[" +
+      s"\n\t\tscala.concurrent.context.minThreads: ${System.getProperty("scala.concurrent.context.minThreads")}" +
+      s"\n\t\tscala.concurrent.context.maxThreads: ${System.getProperty("scala.concurrent.context.maxThreads")}" +
+      s"\n\t\tscala.concurrent.context.numThreads: ${System.getProperty("scala.concurrent.context.numThreads")}" +
+      s"\n\t]" +
+      "\n]"
+    )
+  }
 }
