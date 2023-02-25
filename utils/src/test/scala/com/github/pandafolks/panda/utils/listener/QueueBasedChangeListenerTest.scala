a075c1bfa35220c6ce917b9917789176178051ec
MattSzm <mateuszszmal@gmail.com>
2023-02-25 15:48:02 +0100

    #noissue tests improvements
    Signed-off-by: mszmal <mszmal@atlassian.com>

--- utils/src/test/scala/com/github/pandafolks/panda/utils/listener/QueueBasedChangeListenerTest.scala
+++ utils/src/test/scala/com/github/pandafolks/panda/utils/listener/QueueBasedChangeListenerTest.scala
@@ -6,16 +6,14 @@ import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-import scala.jdk.CollectionConverters._
+import java.util.concurrent.ConcurrentLinkedQueue
 
 final class QueueBasedChangeListenerTest extends AsyncFlatSpec with Matchers with ScalaFutures {
   private implicit val testingScheduler: Scheduler =
     Scheduler.forkJoin(Runtime.getRuntime.availableProcessors(), Runtime.getRuntime.availableProcessors())
 
   "notifyAboutAdd" should "process the items by the overwritten notifyAboutAddInternal method" in {
-    import java.util
-    import java.util.Collections
-    val memo = Collections.synchronizedList(new util.ArrayList[Int])
+    val memo = new ConcurrentLinkedQueue[Int]()
 
     val itemsNumber = 10000
 
@@ -31,14 +29,12 @@ final class QueueBasedChangeListenerTest extends AsyncFlatSpec with Matchers wit
 
     whenReady(request) { _ =>
       while (memo.size() != itemsNumber) {}
-      memo.asScala should be(Range.inclusive(1, itemsNumber).map(_ * 2))
+      memo should contain theSameElementsAs Range.inclusive(1, itemsNumber).map(_ * 2)
     }
   }
 
   it should "be able to handle many concurrent method calls" in {
-    import java.util
-    import java.util.Collections
-    val memo = Collections.synchronizedList(new util.ArrayList[Int])
+    val memo = new ConcurrentLinkedQueue[Int]()
 
     val queue = new QueueBasedChangeListener[Int] {
       protected lazy implicit val scheduler: Scheduler = testingScheduler
@@ -53,14 +49,12 @@ final class QueueBasedChangeListenerTest extends AsyncFlatSpec with Matchers wit
     whenReady(request) { _ =>
       while (memo.size() != 20 * 400) {}
 
-      memo.asScala should contain theSameElementsAs List.fill(20)(Range.inclusive(1, 400)).flatten.map(_ * 3)
+      memo should contain theSameElementsAs List.fill(20)(Range.inclusive(1, 400)).flatten.map(_ * 3)
     }
   }
 
   "notifyAboutRemove" should "process the items by the overwritten notifyAboutRemoveInternal method" in {
-    import java.util
-    import java.util.Collections
-    val memo = Collections.synchronizedList(new util.ArrayList[Int])
+    val memo = new ConcurrentLinkedQueue[Int]()
 
     val itemsNumber = 10000
 
@@ -76,14 +70,12 @@ final class QueueBasedChangeListenerTest extends AsyncFlatSpec with Matchers wit
 
     whenReady(request) { _ =>
       while (memo.size() != itemsNumber) {}
-      memo.asScala should be(Range.inclusive(1, itemsNumber).map(_ * 2))
+      memo should contain theSameElementsAs Range.inclusive(1, itemsNumber).map(_ * 2)
     }
   }
 
   it should "be able to handle many concurrent method calls" in {
-    import java.util
-    import java.util.Collections
-    val memo = Collections.synchronizedList(new util.ArrayList[Int])
+    val memo = new ConcurrentLinkedQueue[Int]()
 
     val queue = new QueueBasedChangeListener[Int] {
       protected lazy implicit val scheduler: Scheduler = testingScheduler
@@ -98,16 +90,13 @@ final class QueueBasedChangeListenerTest extends AsyncFlatSpec with Matchers wit
     whenReady(request) { _ =>
       while (memo.size() != 200 * 200) {}
 
-      memo.asScala should contain theSameElementsAs List.fill(200)(Range.inclusive(1, 200)).flatten.map(_ * 3)
+      memo should contain theSameElementsAs List.fill(200)(Range.inclusive(1, 200)).flatten.map(_ * 3)
     }
   }
 
   it should "work independently from notifyAboutAdd" in {
-
-    import java.util
-    import java.util.Collections
-    val notifyAboutAddMemo = Collections.synchronizedList(new util.ArrayList[Int])
-    val notifyAboutRemoveMemo = Collections.synchronizedList(new util.ArrayList[Int])
+    val notifyAboutAddMemo = new ConcurrentLinkedQueue[Int]()
+    val notifyAboutRemoveMemo = new ConcurrentLinkedQueue[Int]()
 
     val queue = new QueueBasedChangeListener[Int] {
       protected lazy implicit val scheduler: Scheduler = testingScheduler
@@ -119,16 +108,16 @@ final class QueueBasedChangeListenerTest extends AsyncFlatSpec with Matchers wit
 
     val request = Task
       .parZip2(
-        Task.parSequence(List.fill(60)(queue.notifyAboutAdd(Range.inclusive(1, 210)))),
-        Task.parSequence(List.fill(20)(queue.notifyAboutRemove(Range.inclusive(1, 350))))
+        Task.parSequenceUnordered(List.fill(20)(queue.notifyAboutAdd(Range.inclusive(1, 210)))),
+        Task.parSequenceUnordered(List.fill(20)(queue.notifyAboutRemove(Range.inclusive(1, 350))))
       )
       .runToFuture
 
     whenReady(request) { _ =>
-      while (notifyAboutAddMemo.size() != 60 * 210 && notifyAboutRemoveMemo.size() != 20 * 350) {}
+      while (notifyAboutAddMemo.size() != 20 * 210 && notifyAboutRemoveMemo.size() != 20 * 350) {}
 
-      notifyAboutAddMemo.asScala should contain theSameElementsAs List.fill(60)(Range.inclusive(1, 210)).flatten.map(_ * 13)
-      notifyAboutRemoveMemo.asScala should contain theSameElementsAs List.fill(20)(Range.inclusive(1, 350)).flatten.map(_ * 17)
+      notifyAboutAddMemo should contain theSameElementsAs List.fill(20)(Range.inclusive(1, 210)).flatten.map(_ * 13)
+      notifyAboutRemoveMemo should contain theSameElementsAs List.fill(20)(Range.inclusive(1, 350)).flatten.map(_ * 17)
     }
   }
 }
