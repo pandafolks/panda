a91c25b03c4d645e72e545ca0bca4d92efa04811
MattSzm <mateuszszmal@gmail.com>
2023-06-04 18:24:49 +0200

    #50 migrated to rolling logger

---
+++ bootstrap/src/main/scala/com/github/pandafolks/panda/bootstrap/LoggerStartupListener.scala
@@ -0,0 +1,47 @@
+package com.github.pandafolks.panda.bootstrap
+
+import ch.qos.logback.classic.{Level, Logger, LoggerContext}
+import ch.qos.logback.classic.spi.LoggerContextListener
+import ch.qos.logback.core.spi.{ContextAwareBase, LifeCycle}
+import com.github.pandafolks.panda.utils.SystemProperties
+
+final class LoggerStartupListener extends ContextAwareBase with LoggerContextListener with LifeCycle {
+  private var started = false
+
+  @Override
+  def start(): Unit = {
+    if (started) return
+
+    val context = getContext()
+
+    // main logger
+    context.putProperty("MAIN_LOG_FILE_NAME", SystemProperties.mainLogFileName)
+    context.putProperty("MAIN_LOG_MAX_FILE_SIZE", SystemProperties.mainLogMaxFileSize)
+    context.putProperty("MAIN_LOG_MAX_HISTORY_DAYS", SystemProperties.mainLogMaxHistoryInDays)
+    context.putProperty("MAIN_LOG_TOTAL_SIZE_CAP", SystemProperties.mainLogTotalSizeCap)
+
+    // gateway logger
+    context.putProperty("GATEWAY_TRAFFIC_LOG_FILE_NAME", SystemProperties.gatewayTrafficLogFileName)
+    context.putProperty("GATEWAY_TRAFFIC_LOG_MAX_FILE_SIZE", SystemProperties.gatewayTrafficLogMaxFileSize)
+    context.putProperty("GATEWAY_TRAFFIC_LOG_MAX_HISTORY_DAYS", SystemProperties.gatewayTrafficLogMaxHistoryInDays)
+    context.putProperty("GATEWAY_TRAFFIC_LOG_TOTAL_SIZE_CAP", SystemProperties.gatewayTrafficLogTotalSizeCap)
+
+    context.putProperty("LOG_TO_STDOUT", "True")
+
+    started = true
+  }
+
+  override def stop(): Unit = {}
+
+  override def isStarted(): Boolean = started
+
+  override def isResetResistant: Boolean = true
+
+  override def onStart(context: LoggerContext): Unit = {}
+
+  override def onReset(context: LoggerContext): Unit = {}
+
+  override def onStop(context: LoggerContext): Unit = {}
+
+  override def onLevelChange(logger: Logger, level: Level): Unit = {}
+}
--- utils/src/main/scala/com/github/pandafolks/panda/utils/SystemProperties.scala
+++ utils/src/main/scala/com/github/pandafolks/panda/utils/SystemProperties.scala
@@ -6,6 +6,16 @@ object SystemProperties {
   object Defaults {
     final val CONSISTENT_HASHING_STATE_POSITIONS_PER_PARTICIPANT_DEFAULT: Int = 20
     final val CONSISTENT_HASHING_STATE_CLEAR_EMPTY_GROUPS_INTERVAL_IN_HOURS: Int = 12
+
+    final val MAIN_LOG_FILE = "panda"
+    final val MAIN_LOG_MAX_FILE_SIZE = "100MB"
+    final val MAIN_LOG_MAX_HISTORY_IN_DAYS = "60"
+    final val MAIN_LOG_TOTAL_SIZE_CAP = "10GB"
+
+    final val GATEWAY_TRAFFIC_LOG_FILE = "gateway_traffic"
+    final val GATEWAY_TRAFFIC_LOG_MAX_FILE_SIZE = "100MB"
+    final val GATEWAY_TRAFFIC_LOG_MAX_HISTORY_IN_DAYS = "60"
+    final val GATEWAY_TRAFFIC_LOG_TOTAL_SIZE_CAP = "10GB"
   }
 
   def scalaConcurrentContextMinThreads: String = System.getProperty("scala.concurrent.context.minThreads")
@@ -42,4 +52,24 @@ object SystemProperties {
   def consistentHashingStateClearEmptyGroupsIntervalInHours: Int =
     Try { System.getProperty("panda.consistent.hashing.state.clear.empty.groups.interval").toInt }
       .getOrElse(Defaults.CONSISTENT_HASHING_STATE_CLEAR_EMPTY_GROUPS_INTERVAL_IN_HOURS)
+
+  def mainLogFileName: String = Option(System.getProperty("panda.main.log.file.name")).getOrElse(Defaults.MAIN_LOG_FILE)
+
+  def mainLogMaxFileSize: String = Option(System.getProperty("panda.main.log.max.file.size")).getOrElse(Defaults.MAIN_LOG_MAX_FILE_SIZE)
+
+  def mainLogMaxHistoryInDays: String =
+    Option(System.getProperty("panda.main.log.max.history.in.days")).getOrElse(Defaults.MAIN_LOG_MAX_HISTORY_IN_DAYS)
+
+  def mainLogTotalSizeCap: String = Option(System.getProperty("panda.main.log.total.size.cap")).getOrElse(Defaults.MAIN_LOG_TOTAL_SIZE_CAP)
+
+  def gatewayTrafficLogFileName: String =
+    Option(System.getProperty("panda.gateway.traffic.log.file.name")).getOrElse(Defaults.GATEWAY_TRAFFIC_LOG_FILE)
+
+  def gatewayTrafficLogMaxFileSize: String =
+    Option(System.getProperty("panda.gateway.traffic.log.max.file.size")).getOrElse(Defaults.GATEWAY_TRAFFIC_LOG_MAX_FILE_SIZE)
+
+  def gatewayTrafficLogMaxHistoryInDays: String =
+    Option(System.getProperty("panda.gateway.traffic.log.max.history.in.days")).getOrElse(Defaults.GATEWAY_TRAFFIC_LOG_MAX_HISTORY_IN_DAYS)
+  def gatewayTrafficLogTotalSizeCap: String =
+    Option(System.getProperty("panda.gateway.traffic.log.total.size.cap")).getOrElse(Defaults.GATEWAY_TRAFFIC_LOG_TOTAL_SIZE_CAP)
 }
